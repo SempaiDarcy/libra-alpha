640b1b7c7e752188d593b4874073a0d62b9af810
SempaiDarcy <mr_darcy_1@mail.ru>
2024-10-26 04:39:50 +0500

    feat: Enhance App component with theme settings, FiSettings button, and footer
    - Integrated theme mode and color management using local storage.
    - Added settings button with FiSettings icon and dynamic background color.
    - Included the Footer component for consistent layout.
    - Ensured responsive design with fixed sidebar and navbar.

--- src/app.tsx
+++ src/app.tsx
@@ -1,9 +1,32 @@
+import {ChangeEvent, useEffect, useState} from "react";
 import {Sidebar} from "./components/sidebar/sidebar.tsx";
 import {Navbar} from "./components/navbar/navbar.tsx";
+import { FiSettings } from 'react-icons/fi';
+import {Footer} from "./components/footer/footer.tsx";
 
 function App() {
+    const [currentMode, setCurrentMode] = useState<string>('Light');
+    const [currentColor, setCurrentColor] = useState<string>('#03C9D7');
+    const [activeMenu, setActiveMenu] = useState<boolean>(true);
+    const setMode = (e: ChangeEvent<HTMLInputElement>) => {
+        setCurrentMode(e.target.value);
+        localStorage.setItem('themeMode', e.target.value);
+    };
+
+    const setColor = (color: string) => {
+        setCurrentColor(color);
+        localStorage.setItem('colorMode', color);
+    };
+    useEffect(() => {
+        const currentThemeColor = localStorage.getItem('colorMode');
+        const currentThemeMode = localStorage.getItem('themeMode');
+        if (currentThemeColor && currentThemeMode) {
+            setCurrentColor(currentThemeColor);
+            setCurrentMode(currentThemeMode);
+        }
+    }, []);
     return (
-        <div>
+        <div className={currentMode === 'Dark' ? 'dark' : ''}>
             <div className="flex relative dark:bg-main-dark-bg">
                 <div className="fixed right-4 bottom-4" style={{zIndex: '1000'}}>
                     <>
@@ -11,10 +34,10 @@ function App() {
                             type="button"
                             onClick={() => {
                             }}
-                            style={{background: 'blue', borderRadius: '50%'}}
+                            style={{background: currentColor, borderRadius: '50%'}}
                             className="text-3xl text-white p-3 hover:drop-shadow-xl hover:bg-light-gray"
                         >
-                            Иконка
+                            <FiSettings/>
                         </button>
                     </>
 
@@ -26,6 +49,7 @@ function App() {
                     <div className="fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full ">
                         <Navbar/>
                     </div>
+                    <Footer />
                 </div>
             </div>
         </div>
@@ -33,4 +57,3 @@ function App() {
 }
 
 export default App
-
