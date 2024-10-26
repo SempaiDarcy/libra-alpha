7b874d48c7e5c7169828147f29ae44f1604d31b7
SempaiDarcy <mr_darcy_1@mail.ru>
2024-10-26 05:17:22 +0500

    feat: Implement theme settings and integrate ThemeSettings component
    - Updated App component to use the custom hook useStateContext.
    - Added settings button with FiSettings icon for theme management.
    - Introduced ThemeSettings component for changing theme mode and color settings.
    - Updated sidebar display structure based on activeMenu state.
    - Added event handlers for managing theme state.

--- src/app.tsx
+++ src/app.tsx
@@ -1,22 +1,15 @@
-import {ChangeEvent, useEffect, useState} from "react";
+import {useEffect} from "react";
 import {Sidebar} from "./components/sidebar/sidebar.tsx";
 import {Navbar} from "./components/navbar/navbar.tsx";
 import { FiSettings } from 'react-icons/fi';
 import {Footer} from "./components/footer/footer.tsx";
+import { TooltipComponent } from '@syncfusion/ej2-react-popups';
+import {useStateContext} from "./hooks/use-state-context.tsx";
+import {ThemeSettings} from "./components/theme/theme-settings.tsx";
 
 function App() {
-    const [currentMode, setCurrentMode] = useState<string>('Light');
-    const [currentColor, setCurrentColor] = useState<string>('#03C9D7');
-    const [activeMenu, setActiveMenu] = useState<boolean>(true);
-    const setMode = (e: ChangeEvent<HTMLInputElement>) => {
-        setCurrentMode(e.target.value);
-        localStorage.setItem('themeMode', e.target.value);
-    };
+    const { setCurrentColor, setCurrentMode, currentMode, activeMenu, currentColor, themeSettings, setThemeSettings } = useStateContext();
 
-    const setColor = (color: string) => {
-        setCurrentColor(color);
-        localStorage.setItem('colorMode', color);
-    };
     useEffect(() => {
         const currentThemeColor = localStorage.getItem('colorMode');
         const currentThemeMode = localStorage.getItem('themeMode');
@@ -29,27 +22,40 @@ function App() {
         <div className={currentMode === 'Dark' ? 'dark' : ''}>
             <div className="flex relative dark:bg-main-dark-bg">
                 <div className="fixed right-4 bottom-4" style={{zIndex: '1000'}}>
-                    <>
+                    <TooltipComponent
+                        content="Settings"
+                        position='Top'
+                    >
                         <button
                             type="button"
-                            onClick={() => {
-                            }}
-                            style={{background: currentColor, borderRadius: '50%'}}
+                            onClick={() => setThemeSettings(!themeSettings)}
+                            style={{ background: currentColor, borderRadius: '50%' }}
                             className="text-3xl text-white p-3 hover:drop-shadow-xl hover:bg-light-gray"
                         >
-                            <FiSettings/>
+                            <FiSettings />
                         </button>
-                    </>
+
+                    </TooltipComponent>
 
                 </div>
-                <div className="w-72 fixed sidebar dark:bg-secondary-dark-bg bg-white ">
-                    <Sidebar/>
-                </div>
+                {activeMenu ? (
+                    <div className="w-72 fixed sidebar dark:bg-secondary-dark-bg bg-white ">
+                        <Sidebar />
+                    </div>
+                ) : (
+                    <div className="w-0 dark:bg-secondary-dark-bg">
+                        <Sidebar />
+                    </div>
+                )}
                 <div className='dark:bg-main-dark-bg  bg-main-bg min-h-screen md:ml-72 w-full'>
                     <div className="fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full ">
                         <Navbar/>
                     </div>
-                    <Footer />
+                    <div>
+                        {themeSettings && (<ThemeSettings/>)}
+                        роуты
+                    </div>
+                    <Footer/>
                 </div>
             </div>
         </div>
