40550a8f36586118b650b6bb4a5857178bc08653
SempaiDarcy <mr_darcy_1@mail.ru>
2024-10-26 06:15:20 +0500

    feat: enhance Sidebar with navigation and icon integration

--- src/app.tsx
+++ src/app.tsx
@@ -6,6 +6,7 @@ import {Footer} from "./components/footer/footer.tsx";
 import { TooltipComponent } from '@syncfusion/ej2-react-popups';
 import {useStateContext} from "./hooks/use-state-context.tsx";
 import {ThemeSettings} from "./components/theme/theme-settings.tsx";
+import {BrowserRouter} from "react-router-dom";
 
 function App() {
     const { setCurrentColor, setCurrentMode, currentMode, activeMenu, currentColor, themeSettings, setThemeSettings } = useStateContext();
@@ -20,44 +21,46 @@ function App() {
     }, []);
     return (
         <div className={currentMode === 'Dark' ? 'dark' : ''}>
-            <div className="flex relative dark:bg-main-dark-bg">
-                <div className="fixed right-4 bottom-4" style={{zIndex: '1000'}}>
-                    <TooltipComponent
-                        content="Settings"
-                        position='Top'
-                    >
-                        <button
-                            type="button"
-                            onClick={() => setThemeSettings(!themeSettings)}
-                            style={{ background: currentColor, borderRadius: '50%' }}
-                            className="text-3xl text-white p-3 hover:drop-shadow-xl hover:bg-light-gray"
+            <BrowserRouter>
+                <div className="flex relative dark:bg-main-dark-bg">
+                    <div className="fixed right-4 bottom-4" style={{zIndex: '1000'}}>
+                        <TooltipComponent
+                            content="Settings"
+                            position='Top'
                         >
-                            <FiSettings />
-                        </button>
+                            <button
+                                type="button"
+                                onClick={() => setThemeSettings(!themeSettings)}
+                                style={{background: currentColor, borderRadius: '50%'}}
+                                className="text-3xl text-white p-3 hover:drop-shadow-xl hover:bg-light-gray"
+                            >
+                                <FiSettings/>
+                            </button>
 
-                    </TooltipComponent>
+                        </TooltipComponent>
 
+                    </div>
+                    {activeMenu ? (
+                        <div className="w-72 fixed sidebar dark:bg-secondary-dark-bg bg-white ">
+                            <Sidebar/>
+                        </div>
+                    ) : (
+                        <div className="w-0 dark:bg-secondary-dark-bg">
+                            <Sidebar/>
+                        </div>
+                    )}
+                    <div className='dark:bg-main-dark-bg  bg-main-bg min-h-screen md:ml-72 w-full'>
+                        <div className="fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full ">
+                            <Navbar/>
+                        </div>
+                        <div>
+                            {themeSettings && (<ThemeSettings/>)}
+                            {/*роуты*/}
+                        </div>
+                        <Footer/>
+                    </div>
                 </div>
-                {activeMenu ? (
-                    <div className="w-72 fixed sidebar dark:bg-secondary-dark-bg bg-white ">
-                        <Sidebar />
-                    </div>
-                ) : (
-                    <div className="w-0 dark:bg-secondary-dark-bg">
-                        <Sidebar />
-                    </div>
-                )}
-                <div className='dark:bg-main-dark-bg  bg-main-bg min-h-screen md:ml-72 w-full'>
-                    <div className="fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full ">
-                        <Navbar/>
-                    </div>
-                    <div>
-                        {themeSettings && (<ThemeSettings/>)}
-                        {/*роуты*/}
-                    </div>
-                    <Footer/>
-                </div>
-            </div>
+            </BrowserRouter>
         </div>
     )
 }
--- src/components/sidebar/sidebar.tsx
+++ src/components/sidebar/sidebar.tsx
@@ -1,7 +1,23 @@
+import { Link } from 'react-router-dom';
+import { GiClockwork } from "react-icons/gi";
+import {useStateContext} from "../../hooks/use-state-context.tsx";
 export const Sidebar = () => {
-    return (
+    const { activeMenu} = useStateContext();
+       return (
         <div className="ml-3 h-screen md:overflow-hidden overflow-auto md:hover:overflow-auto pb-10">
-            Sidebar
+            {activeMenu && (
+                <>
+                    <div className="flex justify-between items-center">
+                        <Link to="/"
+                              className="items-center gap-3 ml-3 mt-4 flex text-xl font-extrabold tracking-tight dark:text-white text-slate-900">
+                            <div style={{width: '2.5rem', height: '2.5rem', overflow: 'hidden'}}>
+                                <GiClockwork style={{width: '100%', height: '100%', objectFit: 'contain'}}/>
+                            </div>
+                            <h1>Libra</h1>
+                        </Link>
+                    </div>
+                </>
+            )}
         </div>
     );
 };
